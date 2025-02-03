a09155283061da4991189caa2933d7b807e7bbe9
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-04 01:47:42 +0500

    ars8

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-name = input('Enter your name: ')
 
-print(name.capitalize(), 'Nice to meet you!!!')
 
-age = input('How old are you: ')
 
-city = input('Where did you come from: ')
-
-print(age, 'you are old', 'you are living in ', city)
 
