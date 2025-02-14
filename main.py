da4f3525d006f09485e2e7fff2fe6e7f808ddc63
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-14 16:20:01 +0500

    ars1

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+users = [
+    {
+        'user_id': 134,
+        'user_name': 'Arslan'
+    },
+    {
+        'user_id': 831,
+        'user_name': 'Alibaster'
+    }
+]
 
-
+print(len(users))
+print(users[1]['user_name'])
 
 
 
