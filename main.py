7e7cf70bf625259c7f20916d5ddc908403e07316
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-14 18:15:51 +0500

    ars7

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+# pervyy i vtoroy
+my_list = [1, 2, 3, 4, 5, 0]
+
+# my_list.pop(2)
+# print(my_list)
+
+# print(len(my_list)) # 3 zadanie
+
+# my_list.sort()
+# print(my_list)
+
+# my_list.sort(reverse=True)
+# print(my_list)  # 4 zadanie
+
+second_list = ['arslan', 'great']
+
+print(my_list + second_list) # 5 zadanie and 6 zadanie
+
+my_list.extend(second_list)
+
+print(my_list[:-2]) # 7 zadanie
+
+
+
+
 
 
 
