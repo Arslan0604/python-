9736bb1adc4b09dd03bc4b55c30f65d33682e757
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-14 18:00:35 +0500

    ars5

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-my_cars = ['BMW', 'Mercedes', 'Audi', 'Toyota', 'Honda']
+my_nums = [10, 50, 0, 5, 5, 100]
 
-copied_cars = my_cars 
-copied_cars.append('Ford')
+print(len(my_nums))
 
-print(copied_cars)
 
-print(my_cars)
-
-print(id(my_cars) == id(copied_cars))
-
-copied_cars = my_cars[:]
-
-copied_cars.append('Chevrolet')
-
-print(copied_cars)
-print(my_cars)
-
-print(id(my_cars) == id(copied_cars))
 
 
 
