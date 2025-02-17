5d3969f43e794e3361e87523021282a61bd7bdd0
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-17 14:42:28 +0500

    ars3

--- dic.py
+++ dic.py
@@ -1,17 +1,35 @@
-my_disk = {
-    
-}
+my_dict = {}
 
-# print(id(my_disk))
-# print(type(my_disk))
+key_1 = input("Please enter your key 1: ")
+value_1 = input("Please enter your value 1: ")
 
-my_disk['brand'] = 'Aple'
-my_disk['price'] = 344
+my_dict[key_1] = value_1
 
-# print(my_disk)
-# print(id(my_disk))
+key_2 = input("Please enter your key 2: ")
+value_2 = input("Please enter your value 2: ")
 
+my_dict[key_2] = value_2
 
-print(my_disk.get('type'))
- 
+key_3 = input("Please enter your key 3: ")
+value_3 = input("Please enter your value 3: ")
+
+my_dict[key_3] = value_3
+
+key_4 = input("Please enter your key 4: ")
+value_4 = input("Please enter your value 4: ")
+
+my_dict[key_4] = value_4
+
+key_5 = input("Please enter your key 5: ")
+value_5 = input("Please enter your value 5: ")
+
+my_dict[key_5] = value_5
+
+key_6 = input("Please enter your key 6: ")  
+value_6 = input("Please enter your value 6: ")
+
+my_dict[key_6] = value_6
+
+print(my_dict)
+
 
