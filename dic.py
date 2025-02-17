1c1284379c67488a79aca37fccdf2a400da1e46c
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-17 13:29:45 +0500

    ars2

--- dic.py
+++ dic.py
@@ -1,7 +1,17 @@
-my_motobike = {
-    'brand': 'honda',
-    'price': 1000,
-    'encine': 1.6
+my_disk = {
+    
 }
 
-print(my_motobike['price'])
+# print(id(my_disk))
+# print(type(my_disk))
+
+my_disk['brand'] = 'Aple'
+my_disk['price'] = 344
+
+# print(my_disk)
+# print(id(my_disk))
+
+
+print(my_disk.get('type'))
+ 
+
