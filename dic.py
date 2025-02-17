179c8ce31c78ea5d2a5a1a260206c9903b7db612
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-17 10:41:17 +0500

    ars1

---
+++ dic.py
@@ -0,0 +1,7 @@
+my_motobike = {
+    'brand': 'honda',
+    'price': 1000,
+    'encine': 1.6
+}
+
+print(my_motobike['price'])
--- main1.py
+++ main1.py
@@ -1,1 +1,14 @@
-my_list = 
+first_list = [34, False, 'Arslan']
+secod_list = [[3,4], {'j': True}]
+
+merget_list = first_list + secod_list
+print(merget_list)
+
+other_merged_list = first_list.__add__(secod_list)
+print(other_merged_list)
+
+print(first_list)
+print(secod_list)
+
+
+
