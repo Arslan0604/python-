617a645547cb6cf2af88203e3458e337a48cf408
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-18 18:14:18 +0500

    ars3

---
+++ karteji.py
@@ -0,0 +1,18 @@
+post_ids = (123, 332)
+
+post_ids_list = list(post_ids)
+
+post_ids_list.append(444)
+
+print(post_ids_list)
+
+post_ids_tuple = tuple(post_ids_list)
+
+print(post_ids_tuple)
+
+
+
+
+
+
+
