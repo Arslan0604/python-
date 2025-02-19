9977a7e537ba4dae81aee9245a4eacb6eb4c6565
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-19 18:48:41 +0500

    ars2

--- set.py
+++ set.py
@@ -1,6 +1,13 @@
 # dublikaty udalayutsya avtomaticheski v setah 
+# indexov u setov net potomu chto ne uporadochna nabor
+# eslit net magicheskogo getitem
 
-my_fruits = {'apple', 'banana', 'lime'}
-print(my_fruits)
-print(type(my_fruits))
-print(len(my_fruits))
+
+posts_ids = [43, 23, 13, 56, 23]
+
+print(posts_ids.__getitem__(1))
+
+print(posts_ids[1])
+
+
+
