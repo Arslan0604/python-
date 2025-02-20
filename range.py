75578cf86c7b00a5db91d6f43677abca62d9015f
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-20 19:15:41 +0500

    ars2

---
+++ range.py
@@ -0,0 +1,26 @@
+# understanding of range
+# my_range = range(7)
+# print(type(my_range)) 
+# print(my_range)
+# print(list(my_range))
+# print(tuple(my_range))
+# print(set(my_range))
+
+# my_range = range(10, 20, 3)
+# print(type(my_range))
+# print(my_range)
+# print(list(my_range))
+
+# # understanding of index in range
+# my_range = range(10, 20, 3)
+# print(my_range[0])
+# print(my_range[1])
+# print(my_range[2])
+# print(my_range[3])
+# print(my_range[4])
+
+my_range = range(5)
+print(my_range)
+print(type(my_range))
+print(my_range[-1])
+
