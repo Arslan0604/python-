b678a88f279d989a9384d980b4efefd297395945
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-21 18:48:51 +0500

    ars2

--- zip.py
+++ zip.py
@@ -1,11 +1,37 @@
-fruits = ['apple', 'banana', 'lime']
+# sozdaem list 
+# fruits = ['apple', 'banana', 'lime', 'orange']
+# quantities = [100, 70, 50, 20]
+# availability = [True, False, False, True]
+# fruit_qtys_zip = zip(fruits, quantities, availability)
+# print(fruit_qtys_zip)
+# print(type(fruit_qtys_zip))
+# fruit_qtys_list = list(fruit_qtys_zip)
+# print(fruit_qtys_list)
 
-quantities = [100, 70, 50]
+# sozdaem dictionery
+# fruits = ['apple', 'banana', 'lime', 'orange']
+# quantities = [100, 70, 50, 20]
 
-fruit_qtys_zip = zip(fruits, quantities)
+# fruit_qtys_zip = zip(fruits, quantities)
+# print(fruit_qtys_zip)
 
-print(fruit_qtys_zip)
+# fruit_qtys_dict = dict(fruit_qtys_zip)
+# print(fruit_qtys_dict) #{'apple': 100, 'banana': 70, 'lime': 50, 'orange': 20}
 
-fruit_qtys_list = list(fruit_qtys_zip)
+# product = ['laptop', 'air_conditioning', 'tablet']
+# price = [545, 200, 300]
+# product_price_zip = zip(product, price)
+# print(product_price_zip)
+# product_price_list = list(product_price_zip)
 
-print(fruit_qtys_list)
+
+# print(product_price_list)
+
+product = ['laptop', 'air_conditioning', 'tablet']
+price = [545, 200, 300]
+product_price_zip = zip(product, price)
+print(product_price_zip)
+product_price_dict = dict(product_price_zip)
+
+
+print(product_price_dict)
