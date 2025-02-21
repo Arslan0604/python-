72c71c07fdf4ed73ef46bd1bfd9282bd80065d51
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-21 18:26:04 +0500

    ars1

---
+++ zip.py
@@ -0,0 +1,11 @@
+fruits = ['apple', 'banana', 'lime']
+
+quantities = [100, 70, 50]
+
+fruit_qtys_zip = zip(fruits, quantities)
+
+print(fruit_qtys_zip)
+
+fruit_qtys_list = list(fruit_qtys_zip)
+
+print(fruit_qtys_list)
