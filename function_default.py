3bc88b7ff8fda731c25c48ef735d2bfc5533a56b
Arslan Ovezov <arslan.datascience@gmail.com>
2025-02-27 11:23:03 +0500

    ars1

---
+++ function_default.py
@@ -0,0 +1,17 @@
+from datetime import date 
+
+def get_weekday():
+    return date.today().strftime('%A')
+
+def create_new_post(post, weekday=get_weekday()):
+    post_copy = post.copy()
+    post_copy['create_on_weekday'] = weekday
+    return post_copy
+
+intial_post = { 
+    'id' : 12,
+    'author' : 'Arslan',
+}
+
+post_with_weekday = create_new_post(intial_post)
+print(post_with_weekday)
