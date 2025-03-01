0f3b0ec22a9d3aa0302e44bdf52f4a1cc6056d6e
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-01 10:58:45 +0500

    ars1

---
+++ opisanie_function.py
@@ -0,0 +1,12 @@
+def print_number_info(num):
+    
+    if (num % 2) == 0: 
+        print("Num is even")
+    else:
+        print("Num is odd")
+        
+        
+    return num 
+        
+        
+print_number_info
