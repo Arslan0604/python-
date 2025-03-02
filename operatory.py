07d35e45f55920f84e678865099f5b1e817c981b
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-03 00:02:18 +0500

    ars3

--- operatory.py
+++ operatory.py
@@ -1,7 +1,29 @@
-arslan_1 = {10, 'abc', 50, True} # poryadok ne imeet znacheniya
-arslan_2 = {10, 'abc', 50, True}
+# arslan_1 = {10, 'abc', 50, True} # poryadok ne imeet znacheniya
+# arslan_2 = {'abc', 10, 50, True}
 
-arslan_1 == arslan_2
-print(arslan_1 == arslan_2) # True
-print(arslan_1.__eq__(arslan_2)) 
-print(arslan_1 in arslan_2)
+# arslan_1 == arslan_2
+# print(arslan_1 == arslan_2) # True
+# print(arslan_1.__eq__(arslan_2)) # True
+
+# print(arslan_1 is arslan_2) # False
+
+# print('abc' in arslan_1) # True 
+# print(132 in arslan_2) # False 
+# # TypeError: unhashable type: 'list'
+# # print([] in arslan_1)
+
+# lojnye znacheniya 
+# print(bool(0)) 
+# print(bool({})) # dict
+# print(bool([])) # list
+# print(bool()) # Tuple 
+# print(bool(set())) # set 
+# print(bool(range(0))) # range 
+# print(bool('')) # str 
+# print(bool(None))
+
+print(not not['a', 'abc', 10])
+print(bool({}))
+print(bool(set('abc')))
+print(bool('abc'))
+
