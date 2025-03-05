f04f281bf5bb660467ef362438d2ff2b9986807f
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-06 00:49:08 +0500

    ars2

---
+++ operator_rasp_slovar.py
@@ -0,0 +1,13 @@
+grey_button = {
+    'width': 200,
+    'text': 'Buy',
+    'color': 'grey',
+}
+
+red_button = {
+    'color': 'red',
+    **grey_button,
+}
+
+print(red_button) 
+print(grey_button)
