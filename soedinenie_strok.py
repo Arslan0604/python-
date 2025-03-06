00944096b19a502cf739dd8b0982d902659bdd0c
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-06 22:15:29 +0500

    ars2

--- soedinenie_strok.py
+++ soedinenie_strok.py
@@ -1,5 +1,39 @@
-hello = 'Hello'
-world = 'World'
+# hello = 'Hello'
+# world = 'World'
 
-greeting = hello + ' ' + world + '!'
-print(greeting)
+# greeting = hello + ' ' + world + '!'
+# print(greeting)
+
+# formatirovanie strok
+# hello = 'Hello'
+# world = 'World'
+
+# greeting = f"{hello} {world}"
+# print(greeting)
+
+# prictika s +om
+
+# my_name = 'Arslan'
+# my_hobby = 'programming and swimming'
+# time = 10
+
+# info = my_name + ' likes ' + my_hobby + ' at ' + str(time) + ' o\'clock'
+# print(info)
+
+# prictika s formatirovaniem
+
+# my_name = 'Arslan'
+# my_hobby = 'programming and swimming'
+# time = 10
+
+# info = f"{my_name} likes {my_hobby} at {str(time)} o'clock"
+# print(info.upper())
+
+# jonno = 'Bek'
+# number = 10 
+# you = [1, 4]
+# dic = {'jon': 'lato', '10': 20}
+
+# info = f"{jonno} plays football every {number} days, and he has {you[0]} and {you[1]} balls, and he has {dic['jon']} and {dic['10']} balls"
+
+# print(info)
