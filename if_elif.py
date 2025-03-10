f96996ec8962ceb02bc33adaa434e6cb0dc2daa9
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-11 00:31:45 +0500

    ars2

---
+++ if_elif.py
@@ -0,0 +1,23 @@
+# my_number = 24
+
+# if my_number > 0:
+#     print(my_number, "is positive number")
+
+# num_one = 10 
+# num_two  = 5.2
+
+# if (num_one > 0 and 
+#     num_two > 0 and 
+#     isinstance(num_one, int) and 
+#     isinstance(num_two, float)):
+    
+#     print("Both numbers are ints and positive")
+
+
+    
+    
+
+
+    
+
+    
