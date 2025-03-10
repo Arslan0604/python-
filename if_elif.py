845fbbb484fec65806027d8b5969e318e99ca982
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-11 02:03:01 +0500

    ars4

--- if_elif.py
+++ if_elif.py
@@ -60,6 +60,18 @@ def route_info(route):
         route['time']}"
         
     return "No distance info is available"
+
+# drugoy sposob reshenie s elif 
+# def route_info(route):
+#     if ('distance' in route) and (type(route['distance']) == int):
+#         route_info = f"Distance to your distanation is {route['distance']}"
+#     elif ('speed' in route) and ('time' in route):
+#         route_info =  f"Distance to your destination is {route['speed'] * 
+#         route['time']}"
+#     else:   
+#         route_info = "No distance info is available"
+    
+#     return route_info
     
 print(route_info({'distance': 14}))
 # Distance to your distanation is 14
