6b6c11453d63d2481412662fc2aa25cd94ba5128
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-15 20:48:07 +0500

    ars3

--- loop.py
+++ loop.py
@@ -73,11 +73,8 @@
 
    
 def filter_list(list_to_filter, value_type):
-    def check_element_type(elem):
-        print(type(elem))
-        return type(elem) is value_type
-    
-    return list(filter(check_element_type, list_to_filter))
+    return list(filter(lambda elem: type(elem) is value_type, 
+                       list_to_filter))
 
 res = filter_list([1, 33, 'abc',True, 4.3], int)
 print(res)
