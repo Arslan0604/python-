694be309730ecd3ddd9b7cb292a4d6b54837dfce
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-15 18:41:00 +0500

    ars2

---
+++ loop.py
@@ -0,0 +1,25 @@
+# this list for loop
+# my_list = [True, 10, 'abc', {}] 
+# for elem in my_list:
+#     print(elem)
+
+# this for tuple(carteje)
+# video_info = ('1920x1080', True, 27)
+# for elem in video_info:
+#     print(elem)
+
+# for in for dictioneries 
+# my_object = {
+#     'x': 10,
+#     'y': True,
+#     'z': 'abc'
+# }
+# for key in my_object:
+#     print(key, my_object[key])
+
+my_dict = {'id': 432, 'title': 'test'}
+for key in my_dict:
+    print(type(key))
+    print(key)
+    print(my_dict[key])
+    
