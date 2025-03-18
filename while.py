083612af4f0d840d91a9d2d5d292acbdfb3283ed
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-18 09:06:03 +0500

    ars2

--- while.py
+++ while.py
@@ -10,13 +10,30 @@
 #     if answer == 'no':
 #         break
     
-import random
+# import random
 
-random_num = random.randint(1, 5)
+# random_num = random.randint(1, 5)
+# while True:
+#     num = int(input('Guess the number from 1 to 5: '))
+#     if num != random_num: 
+#         print('Try again...')
+#         continue
+#     print('Congratulations!', random_num)
+#     break
+
+# task 
+
 while True:
-    num = int(input('Guess the number from 1 to 5: '))
-    if num != random_num: 
-        print('Try again...')
+    try: 
+        num_one = float(input("please enter number one: "))
+        num_two = float(input("please enter number two: "))
+    except ValueError as e:
+        print(e)
+        print("you must enter numbers")
         continue
-    print('Congratulations!', random_num)
-    break
+        
+    print(num_one / num_two)
+    
+    answer = input("do you want to continue? (yes/no): ")
+    if answer == 'no':
+        break
