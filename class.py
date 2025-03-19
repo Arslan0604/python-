66db1c9d3799f0bdddc3728c24798a2625b604e2
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-19 22:18:12 +0500

    ars1

--- class.py
+++ class.py
@@ -9,52 +9,56 @@
 # my_car.move()
 # my_car.stop()
 
-# class Comment:
-#     def __init__(self, text, initial_votes_qty=0):
-#         self.text = text
-#         self.votes_qty = initial_votes_qty
+class Comment:
+    def __init__(self, text, initial_votes_qty=0):
+        self.text = text
+        self.votes_qty = initial_votes_qty
         
-#     def upvote(self, qty):
-#         self.votes_qty += qty
+    def upvote(self, qty):
+        self.votes_qty += qty
         
-#     def reset_votes_qty(self):
-#         self.votes_qty = 0
+    def reset_votes_qty(self):
+        self.votes_qty = 0
         
-# first_comment = Comment("my comment")
+first_comment = Comment("my comment")
 
-# print(first_comment.votes_qty)
+print(first_comment.votes_qty)
 
-# first_comment.upvote(10)
-# first_comment.upvote(20)
-# print(first_comment.votes_qty)
+first_comment.upvote(10)
+first_comment.upvote(20)
+print(first_comment.votes_qty)
 
-# first_comment.reset_votes_qty()
+first_comment.reset_votes_qty()
 
-# print(first_comment.votes_qty)
+print(first_comment.votes_qty)
+
+
 
 # task 1 
-class Image:
-    def __init__(self, resolution, title, extention):
-        self.resolution = resolution
-        self.title = title 
-        self.extention = extention
+# class Image:
+#     def __init__(self, resolution, title, extention):
+#         self.resolution = resolution
+#         self.title = title 
+#         self.extention = extention
      
-    def resize(self, new_resolution):
-        self.resolution = new_resolution 
+#     def resize(self, new_resolution):
+#         self.resolution = new_resolution 
         
-    def __str__(self):
-        return f"{self.title}. {self.extention}"
+#     def __str__(self):
+#         return f"{self.title}. {self.extention}"
         
-first_img = Image('1920x1080', 'My dog', 'jpg')
+# first_img = Image('1920x1080', 'My dog', 'jpg')
 
-print(first_img.resolution)
-print(first_img.title)
-print(first_img.extention)
+# print(first_img.resolution)
+# print(first_img.title)
+# print(first_img.extention)
 
-first_img.resize('4000x3000')
+# first_img.resize('4000x3000')
 
-print(first_img.resolution)
+# print(first_img.resolution)
 
-second_img = Image('8000x5000', 'jango', 'lato')
+# second_img = Image('8000x5000', 'jango', 'lato')
 
-print(first_img)
+# print(first_img)
+
+
