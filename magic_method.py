d00ba97aefbc01b1201f3a4afff344044ca8d7d8
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-22 23:57:19 +0500

    ars3

--- magic_method.py
+++ magic_method.py
@@ -27,7 +27,7 @@ class ExtendedList(list):
         print(f"List has {len(self)} elements")
         
 custom_list = ExtendedList([3, 5, 2])
-custom_list.print_list_info
 
-print(custom_list)
+print(object.__subclasses__())
+
   
