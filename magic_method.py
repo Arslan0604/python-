5ce331a52a326296b3849fee329d0ca966a4f9f7
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-22 23:47:57 +0500

    ars1

--- magic_method.py
+++ magic_method.py
@@ -1,22 +1,33 @@
-class Comment:
-    def __init__(self, text):
-        self.text = text
-        self.votes_qty = 0 
+# class Comment:
+#     def __init__(self, text):
+#         self.text = text
+#         self.votes_qty = 0 
         
-    def upvote(self):
-        self.votes_qty += 1
+#     def upvote(self):
+#         self.votes_qty += 1
         
-    def __add__(self, other):
-        return (f"{self.text} {other.text}",
-                self.votes_qty + other.votes_qty)
+#     def __add__(self, other):
+#         return { 
+#                 'text': f"{self.text} {other.text}",
+#                 'total_votes_qty' : self.votes_qty + other.votes_qty
+#         }
         
-first_comment = Comment("First comment")
-first_comment.upvote()
-second_comment = Comment("Second comment")
-second_comment.upvote()
+# first_comment = Comment("First comment")
+# first_comment.upvote()
+# second_comment = Comment("Second comment")
+# second_comment.upvote()
+# second_comment.upvote()
 
-print(first_comment + second_comment)
 
+# print(first_comment + second_comment)
+# print(first_comment == second_comment)
 
+class ExtendedList(list):
+    def print_list_info(self):
+        print(f"List has {len(self)} elements")
         
+custom_list = ExtendedList([3, 5, 2])
+custom_list.print_list_info
+
+print(custom_list)
   
