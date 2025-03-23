36a6e1cb3a9b5607e61158276a68204855b9d79c
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-23 19:37:21 +0500

    ars1

---
+++ decorator.py
@@ -0,0 +1,21 @@
+def decorator_function(original_fn):
+    def wrappper_function(*args, **kwargs):
+        # some actions before execution of the original_fn
+        print("Executed before function")
+        
+        result = original_fn(*args, **kwargs)
+        
+        # some actions after execution of the original_fn
+        print("Executed after function")
+        
+        # return result
+
+    return wrappper_function
+
+@decorator_function
+def my_function(a, b):
+    print("this is my function!")
+    return (a, b)
+    
+result = my_function(100, 40)
+print(result)
