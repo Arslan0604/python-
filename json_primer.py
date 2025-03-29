06a6068ec024854bfc49f1648997c46544b42f9c
Arslan Ovezov <arslan.datascience@gmail.com>
2025-03-29 19:41:09 +0500

    ars1

---
+++ json_primer.py
@@ -0,0 +1,13 @@
+from json import loads
+import json
+
+json_str = '{"id": 235, "brand": "Nike", "qty":84, "status": {"isForSale": true}}'
+
+sneakers = json.loads(json_str)
+
+print(type(sneakers))
+
+
+print(sneakers['brand'])
+print(sneakers['qty'])
+print(sneakers['status']['isForSale'])
