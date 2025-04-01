effef19e81d9775112f6ed75f7a91ccd3cd004c7
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-01 20:01:28 +0500

    ars1

---
+++ rabota_file.py
@@ -0,0 +1,20 @@
+# from os import path
+
+# print(path.abspath('.'))
+# from pathlib import Path 
+
+
+# my_dir = Path('/Users')/'macbookpro'/'Desktop'/'Python'/'django'
+
+# if not my_dir.exists():  # dobavlyaet papku
+#     my_dir.mkdir()
+    
+    
+# if my_dir.exists():      # udalyaet papku 
+#     my_dir.rmdir()
+
+
+# from pathlib import Path
+
+# print(type(Path('.')))
+
