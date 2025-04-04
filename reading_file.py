8aca1bb23af4b7ec48943ea0da6e6a0b2eb7fdb8
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-04 16:15:39 +0500

    ars4

--- reading_file.py
+++ reading_file.py
@@ -1,15 +1,7 @@
+from pathlib import Path 
 
-# sozdanie zapisya v fail
-with open('test.txt', 'w') as test_file:
-    test_file.write('First string\n')
-    test_file.write('Second string\n')
-    test_file.write('Third string\n')
-    
-# chtenie iz faila
-with open('test.txt') as test_file:
-    lines = test_file.readlines()
-    for line in lines:
-        print(line)
+Path('test.txt').unlink()  # удаляет файл
+
     
 
 
