b3c79ea8f7a8d009cf38959657361465673fb842
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-04 16:04:00 +0500

    ars3

--- reading_file.py
+++ reading_file.py
@@ -1,5 +1,16 @@
-test_file = open('test.txt', 'w')
 
-test_file.write('First string\n')
-
+# sozdanie zapisya v fail
+with open('test.txt', 'w') as test_file:
+    test_file.write('First string\n')
+    test_file.write('Second string\n')
+    test_file.write('Third string\n')
     
+# chtenie iz faila
+with open('test.txt') as test_file:
+    lines = test_file.readlines()
+    for line in lines:
+        print(line)
+    
+
+
+
