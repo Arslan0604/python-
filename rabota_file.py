1fc7d64985ddc9d45f6dee38ffa9d05636f0af19
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-04 14:45:53 +0500

    ars1

--- rabota_file.py
+++ rabota_file.py
@@ -18,3 +18,50 @@
 
 # print(type(Path('.')))
 
+# from pathlib import Path
+
+# cwd = Path('.')
+
+# print(isinstance(cwd, Path))  # True
+# print(type(cwd))
+
+from pathlib import Path
+# file_path = Path('test.txt')
+
+# print ([m for m in dir(file_path) if not m.startswith('_')]) 
+
+# print(Path.cwd())  # your directory 
+
+# print(Path('usr').joinpath('local').joinpath('bin'))
+# # or 
+# print(Path('urs') / 'local' / 'bin')
+
+# print(Path('main.py').exists())  # True
+
+# print(Path('/Users/macbookpro/Desktop').exists())  # True
+
+# print(Path('other.py').exists())  # False
+
+# print(Path('main.py').is_file())  # True
+# print(Path('../python').is_file())  # False
+# print(Path('../python').is_dir()) # False
+
+# for f in Path('.').iterdir():
+#     print(f)
+    
+
+# from os import path
+
+# print(path.abspath('.'))
+
+
+from pathlib import Path
+
+
+
+cwd = Path('.')
+
+print(isinstance(cwd, Path))
+print(type(cwd))
+
+print(Path.__subclasses__())
