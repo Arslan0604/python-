062464655d3e7ad496448398a6802e5a1976f60e
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-10 14:39:13 +0500

    ars2

--- modul_datetime.py
+++ modul_datetime.py
@@ -1,14 +1,9 @@
 # from datetime import date
-
-
 # my_date  = date(2100, 4, 13)
-
 # print(my_date)
-
 # print(my_date.isocalendar())
 
 # from datetime import time
-
 # my_time = time(15, 54, 32)
 # print(my_time)
 # print(my_time.hour)
@@ -16,7 +11,9 @@
 # print(my_time.second)
 
 from datetime import datetime
-
 my_datetime = datetime(2102, 12, 13, 12, 59, 12, 2133)
-
 print(my_datetime)
+print(my_datetime.year)
+print(my_datetime.hour)
+print(my_datetime.second)
+print(my_datetime.now().microsecond)
