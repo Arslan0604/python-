92ee02369bb1529b0cd1eae62381ca43038e6d04
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-10 15:21:56 +0500

    ars3

--- modul_datetime.py
+++ modul_datetime.py
@@ -10,10 +10,21 @@
 # print(my_time.minute)
 # print(my_time.second)
 
-from datetime import datetime
+from datetime import datetime, timedelta
 my_datetime = datetime(2102, 12, 13, 12, 59, 12, 2133)
-print(my_datetime)
-print(my_datetime.year)
-print(my_datetime.hour)
-print(my_datetime.second)
-print(my_datetime.now().microsecond)
+# print(my_datetime)
+# print(my_datetime.year)
+# print(my_datetime.hour)
+# print(my_datetime.second)
+# print(my_datetime.now().microsecond)
+
+# print(my_datetime.strftime('%d-%b-%Y')) # 3-Dec-2102
+# print(my_datetime.strftime('%d-%b-%Y %H:%M:%S' )) # 13-Dec-2102 12:59:12
+
+# date_str = '10/12/2222'
+
+# converted_date = datetime.strptime(date_str, '%d/%m/%Y')
+
+# print(converted_date)
+
+print(my_datetime + timedelta(days=100, minutes=120, hours=2)) # ty mojesh i minusovat toje 
