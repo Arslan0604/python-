698e4d2b9d26c99b8ad024e280f78a905afa545a
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-11 23:27:50 +0500

    ars4

--- modul_int.py
+++ modul_int.py
@@ -8,3 +8,8 @@ print(random.choice('abcd'))
 print(random.choice([1,10,4]))
 
 print(random.choices([1,10,4, 43, 44], k = 2))
+
+my_list = [1,10,4, 43, 44]
+
+print(random.shuffle(my_list))
+print(my_list)
