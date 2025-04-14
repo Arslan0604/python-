0a16af86863c8f2cfe5bbde49baf0ce9367c1f2d
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-14 23:33:51 +0500

    ars4

--- password.py
+++ password.py
@@ -1,10 +1,11 @@
 import secrets
 import string 
 
-print(string.ascii_letters)
-print(string.ascii_lowercase)
-print(string.ascii_uppercase)
-print(string.digits)
+print(string.ascii_letters) # all_letters
+print(string.ascii_lowercase) # lowecases 
+print(string.ascii_uppercase) # uppercases
+print(string.digits) # digits 
+print(string.punctuation) # simbols
 
 
-print(string.ascii_letters + string.digits)
+print(string.ascii_letters + string.digits) # concat
