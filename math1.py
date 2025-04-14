cec959cb6a0b01f8520b77c632791e47ba445a12
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-15 00:18:19 +0500

    ars2

--- math1.py
+++ math1.py
@@ -1,4 +1,28 @@
 import math
 
-print(math.pi)
-print(math.e)
+# print(math.pi)
+# print(math.e)
+
+# print(math.sqrt(25))
+
+# print(math.log(100))
+
+# print(math.factorial(10))
+
+# print(dir(math))
+
+def calc_factorial(num):
+    if type(num) is not int:
+        raise TypeError("Number must be integer")
+    if num <= 0:
+        raise ValueError("Number must be positive")
+    
+    if num == 1:
+        return 1
+    return calc_factorial(num - 1) * num
+    
+print(calc_factorial(10))
+print(math.factorial(10))
+
+
+    
