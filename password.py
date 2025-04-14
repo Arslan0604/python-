386b9a043a56612df7dacb3db30bbab72db212a5
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-14 23:43:06 +0500

    ars5

--- password.py
+++ password.py
@@ -1,11 +1,15 @@
 import secrets
 import string 
 
-print(string.ascii_letters) # all_letters
-print(string.ascii_lowercase) # lowecases 
-print(string.ascii_uppercase) # uppercases
-print(string.digits) # digits 
-print(string.punctuation) # simbols
+all_chars = string.ascii_letters + string.digits + string.punctuation
+
+print(''.join(secrets.choice(all_chars) for i in range(15))) # vybor 
+
+# print(string.ascii_letters) # all_letters
+# print(string.ascii_lowercase) # lowecases 
+# print(string.ascii_uppercase) # uppercases
+# print(string.digits) # digits 
+# print(string.punctuation) # simbols
 
 
-print(string.ascii_letters + string.digits) # concat
+# print(string.ascii_letters + string.digits + string.punctuation) # concat
