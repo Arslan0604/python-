efb2285debaaf99f212a877fa2da675debda8345
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-17 20:39:54 +0500

    ars3

--- regular_modul.py
+++ regular_modul.py
@@ -41,6 +41,8 @@
 
 import re # vstrinyy modul
 
+test_password = 'ABASasd!3432423'
+
 def check_password(password):
     length_pattern = re.compile(r"\S{8,}") 
     lowercase_pattern = re.compile(r"[a-z]+")
@@ -48,10 +50,23 @@ def check_password(password):
     number_pattern = re.compile(r"[0-9]+")
     spacial_symbol_pattern = re.compile(r"[@#?!*^]+")
     
+    if not re.fullmatch(length_pattern, password):
+        return (False, "Password must have at least 8 symbols")
+    
+    if not re.fullmatch(lowercase_pattern, password):
+        return (False, "Password must have at least one lowercase letter")
+    
+    if not re.fullmatch(uppercase_pattern, password):
+        return (False, "Password must have at least one uppercase letter")
+    
+    if not re.fullmatch(number_pattern, password):
+        return (False, "Password must have at least one number")
+    
+    if not re.fullmatch(spacial_symbol_pattern, password):
+        return (False, "Password must have at least one symbol @#?!*^")
     
     
-    
-    
+print(check_password(test_password))
     
 
 
