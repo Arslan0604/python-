6c0b7591f62503ead4bc4440850714411d008bd0
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-20 13:36:10 +0500

    ars4

--- email_ars.py
+++ email_ars.py
@@ -1,12 +1,17 @@
 from email.message import EmailMessage
 import smtplib
+from string import Template
+from pathlib import Path
 
 my_email = EmailMessage()
 
+html_template = Template(Path("templates/index.html").read_text())
+html_content = html_template.substitute({'name': 'Arslan', 'date': 'Tomorrow'})
+
 my_email['from'] = 'Arslan'
-my_email['to']   = 'test@gmail.com'
-my_email['subject'] = 'hello from python'
-my_email.set_content("Hey! How are you doing?")
+my_email['to']   = 'friend_jonny@gmail.com'
+my_email['subject'] = 'Let us go out'
+my_email.set_content(html_content, 'html')
 
 with smtplib.SMTP(host='localhost', port=2525) as smtp_server:
     smtp_server.ehlo()
