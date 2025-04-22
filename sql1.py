c5b40df34b827a2d4e6c7a3b292ac48cd2ed17fb
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-22 09:28:45 +0500

    ars4

--- sql1.py
+++ sql1.py
@@ -2,9 +2,18 @@ import sqlite3
 
 DB_NAME = "sqlite_db.db"
 # napolnenie bazy dannyh
+
+courses = [
+    (235, "JavaScript course", 99, 54),
+    (43, "C++ course", 12, 43),
+    (54, "Java course", 89, 21),
+    (44, "Ruby course", 77, 11)
+]
+
 with sqlite3.connect(DB_NAME) as sqlite_conn:
     sql_request = "INSERT INTO courses VALUES(?, ?, ?, ?)"
-    sqlite_conn.execute(sql_request, (23, "Python course", 100, 4))
+    for course in courses:
+        sqlite_conn.execute(sql_request, course)
     sqlite_conn.commit()
 
 # Create a new table in the database:
