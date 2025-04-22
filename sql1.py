baa17b62e7f7aea0948c5b44abb34862fe1aa187
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-22 09:20:53 +0500

    ars3

--- sql1.py
+++ sql1.py
@@ -1,16 +1,21 @@
 import sqlite3
 
 DB_NAME = "sqlite_db.db"
-
-
-
+# napolnenie bazy dannyh
 with sqlite3.connect(DB_NAME) as sqlite_conn:
-    sql_request = """CREATE TABLE IF NOT EXISTS courses (
-        id integer PRIMARY KEY,
-        title text NOT NULL,
-        student_qty integer,
-        reviews integer
-    );"""
-    sqlite_conn.execute(sql_request)
+    sql_request = "INSERT INTO courses VALUES(?, ?, ?, ?)"
+    sqlite_conn.execute(sql_request, (23, "Python course", 100, 4))
+    sqlite_conn.commit()
+
+# Create a new table in the database:
+
+# with sqlite3.connect(DB_NAME) as sqlite_conn:
+#     sql_request = """CREATE TABLE IF NOT EXISTS courses (
+#         id integer PRIMARY KEY,
+#         title text NOT NULL,
+#         student_qty integer,
+#         reviews integer
+#     );"""
+#     sqlite_conn.execute(sql_request)
     
     
