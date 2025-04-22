c385d30d7668b185193017be957d269973f2e7c3
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-22 09:12:31 +0500

    ars2

--- sql1.py
+++ sql1.py
@@ -2,6 +2,15 @@ import sqlite3
 
 DB_NAME = "sqlite_db.db"
 
+
+
 with sqlite3.connect(DB_NAME) as sqlite_conn:
-    print(sqlite_conn)
-    print(sqlite3.version)
+    sql_request = """CREATE TABLE IF NOT EXISTS courses (
+        id integer PRIMARY KEY,
+        title text NOT NULL,
+        student_qty integer,
+        reviews integer
+    );"""
+    sqlite_conn.execute(sql_request)
+    
+    
