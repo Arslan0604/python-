5d63f104255c6fd987848fc010f8d34ede46282b
Arslan Ovezov <arslan.datascience@gmail.com>
2025-04-24 23:52:17 +0500

    ars7

--- find_sys.py
+++ find_sys.py
@@ -7,7 +7,9 @@ if len(sys.argv) < 3:
     raise IOError("You must provide username and password")
 
 
-username = sys.argv[1]
-password = sys.argv[2]
+# username = sys.argv[1]
+# password = sys.argv[2]
+
+filename, username, password = sys.argv
 
 print(username, password)
